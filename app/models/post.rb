f0a3ef4f76208561735f83c561857160552c37e6
Ken Henderson <kenderson_MBP@kenderson-mbp.local>
2010-01-01 10:22:23 -0500

    commiting advanced use of users and allowing partners to edit themselves

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates_presence_of :body, :title
   has_many :comments
   belongs_to :user
-  named_scope :recent, :order => 'created_at DESC', :limit => 3  
+  named_scope :recent, :order => 'created_at DESC', :limit => 2
   named_scope :ordered, :order => 'created_at DESC'
 
 end
