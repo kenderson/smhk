74bf559e493b594cb1a45cca31a9b2d8e1b174d1
Ken Henderson <kenderson_MBP@kenderson-mbp.local>
2010-01-17 12:21:05 -0500

    application fix

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,26 @@
 var map_switching_time = 2000;
 jQuery(document).ready(function($) {
+  $('[tooltip]').each(function() {
+    $(this).qtip({
+      content: $(this).attr('tooltip'),
+      position: {
+        corner: {
+          target: 'topRight',
+          tooltip: 'bottomLeft'
+        }
+      },
+      style: {
+        border: {
+          width: 5,
+          radius: 10
+        },
+        padding: 10, 
+        textAlign: 'center',
+        tip: true, // Give it a speech bubble tip with automatic corner detection
+        name: 'dark' // Style it according to the preset 'cream' style
+      }
+    });
+  });
   $('.partners_menu_flyout').hover(  
     function () {$("li.partners_menu a.parent_a").addClass("grey_color");},   
     function () {$("li.partners_menu a.parent_a").removeClass("grey_color");}  
